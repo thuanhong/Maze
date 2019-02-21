5f45b4975c027935a584cc7f911d0bdad5538351
Guest <you@example.com>
2019-02-21 18:24:27 +0700

    add BFS

---
+++ BFS/find.py
@@ -0,0 +1,47 @@
+from collections import deque
+from string import ascii_uppercase
+
+
+class node():
+    def __init__(self, parent=None, pos=None):
+        self.parent = parent
+        self.pos = pos
+
+    def __eq__(self, other):
+        return self.pos == other.pos
+
+
+def find_path(maze, start):
+    start_node = node(None, start)
+
+    open_list = deque([start_node])
+    closed_list = []
+    # handle main
+    while len(open_list) > 0:
+        current_pos = open_list.popleft()
+
+        if current_pos in closed_list:
+            continue
+
+        closed_list.append(current_pos)
+
+        if (maze[current_pos.pos[0]][current_pos.pos[1]] == '!' and
+           len(path) < 17)\
+           or maze[current_pos.pos[0]][current_pos.pos[1]] == 'o':
+            path = []
+            current = current_pos
+            while current is not None:
+                path.append(current.pos)
+                current = current.parent
+            return path[::-1]
+
+        for new in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+            index_1 = current_pos.pos[0] + new[0]
+            index_2 = current_pos.pos[1] + new[1]
+            temp_node = (index_1, index_2)
+            if maze[temp_node[0]][temp_node[1]] == '#' or\
+               maze[temp_node[0]][temp_node[1]] in ascii_uppercase:
+                continue
+            new_node = node(current_pos, temp_node)
+            if new_node not in closed_list:
+                open_list.append(new_node)
---
+++ BFS/maze_ia.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import sys
+from find import *
+from time import time
+
+
+def get_A(maze):
+    '''
+    get coordinate of coin and player
+    '''
+    for x in range(len(maze)):
+        for j, y in enumerate(maze[x]):
+            try:
+                if y == 'A':
+                    return (x, j)
+            except IndexError:
+                continue
+    return 0, 0
+
+
+def get_maze():
+    '''
+    get maze from VM
+    '''
+    board = []
+    while True:
+        data = sys.stdin.readline()
+        if data[0] != '#':
+            break
+        board.append(data)
+    return board
+
+
+def moved(path):
+    '''
+    convert from coordinate to step
+    '''
+    if path[1][0] - path[0][0] == 1:
+        return 'MOVE DOWN\n\n'
+    elif path[1][0] - path[0][0] == -1:
+        return 'MOVE UP\n\n'
+    elif path[1][1] - path[0][1] == 1:
+        return 'MOVE RIGHT\n\n'
+    elif path[1][1] - path[0][1] == -1:
+        return 'MOVE LEFT\n\n'
+
+
+if __name__ == '__main__':
+    data = sys.stdin.readline()
+    while data != '':
+        if 'MAZE' in data:
+            maze = get_maze()
+            start = get_A(maze)
+            started = time()  # start calculate time run
+            move = find_path(maze, tuple(start))  # find pathing
+            sys.stderr.write(str(time()-started)+'\n')  # print time run
+            f = open('debu', 'w')
+            f.write(str(move))
+            sys.stdout.write(moved(move))  # print step
+        elif "HELLO" in data:
+            sys.stdout.write('I AM A\n\n')
+        elif 'YOU' in data:
+            sys.stdout.write('OK\n\n')
+
+        data = sys.stdin.readline()
